a90c3520a4b910275d6d56b7c76c5fd91dd41bea
ProgrammerJayT <theanthem8@gmail.com>
2023-07-16 04:57:01 +0200

    Added icon, and dummy function onpress

--- app/src/components/MapView.tsx
+++ app/src/components/MapView.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View} from 'react-native';
+import {Alert, TouchableOpacity, View} from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 import styles from '../globals/styles';
 import {mapboxAccessToken} from '../constants/mapboxAcessToken';
@@ -14,27 +14,46 @@ const MapView = ({
   zoomLevel,
   zoomEnabled,
 }) => {
+  if (!coords) {
+    return null;
+  }
+
+  // Define the URL of the online icon
+  const onlineIconUrl =
+    'https://cdn-icons-png.flaticon.com/128/11031/11031000.png';
+
+  const alertCoords = () => {
+    Alert.alert('Longitude: ' + coords.lng + '\nLatitude: ' + coords.lat);
+  };
+
   return (
     <View style={styles.container}>
-      {coords && (
-        <Mapbox.MapView
-          style={styles.map}
-          pitchEnabled={true}
-          rotateEnabled={false}
-          zoomEnabled={zoomEnabled ?? true}>
-          <Mapbox.Camera
-            animationMode={animation ?? 'easeTo'}
-            animationDuration={animationDuration ?? 500}
-            zoomLevel={zoomLevel ?? 17}
-            centerCoordinate={[coords.lng, coords.lat]}
-          />
+      <Mapbox.MapView
+        style={styles.map}
+        pitchEnabled={true}
+        rotateEnabled={false}
+        zoomEnabled={zoomEnabled ?? true}>
+        <Mapbox.Camera
+          animationMode={animation ?? 'easeTo'}
+          animationDuration={animationDuration ?? 500}
+          zoomLevel={zoomLevel ?? 17}
+          centerCoordinate={[coords.lng, coords.lat]}
+        />
 
-          <Mapbox.PointAnnotation
-            id="location"
-            coordinate={[coords.lng, coords.lat]}
+        <Mapbox.ShapeSource
+          id="location"
+          shape={{type: 'Point', coordinates: [coords.lng, coords.lat]}}
+          onPress={alertCoords}>
+          <Mapbox.SymbolLayer
+            id="locationSymbol"
+            style={{
+              iconImage: onlineIconUrl,
+              iconSize: 0.4,
+              iconAllowOverlap: true,
+            }}
           />
-        </Mapbox.MapView>
-      )}
+        </Mapbox.ShapeSource>
+      </Mapbox.MapView>
     </View>
   );
 };
