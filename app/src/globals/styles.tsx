05dcd6d15dee90d1a565d48c49d50078e1810cde
ProgrammerJayT <theanthem8@gmail.com>
2023-07-16 04:34:52 +0200

    Created global styles

---
+++ app/src/globals/styles.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+
+  container: {
+    flex: 1,
+  },
+});
