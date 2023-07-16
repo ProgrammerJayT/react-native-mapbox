7888e31a7d3d141635ce126efd4860ba3d1fde35
ProgrammerJayT <theanthem8@gmail.com>
2023-07-16 03:23:21 +0200

    Implemented listener

---
+++ app/src/utilities/LocationListener.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+import {useEffect} from 'react';
+import Geolocation from '@react-native-community/geolocation';
+
+const LocationListener = ({onCoordsReceived}) => {
+  useEffect(() => {
+    console.log('Listening to location changes');
+
+    const watchId = Geolocation.watchPosition(
+      position => {
+        console.log('Position', position);
+        onCoordsReceived(position.coords.latitude, position.coords.longitude);
+      },
+      error => console.log(error),
+      {enableHighAccuracy: true, timeout: 30000, maximumAge: 1000},
+    );
+
+    return () => {
+      Geolocation.clearWatch(watchId);
+    };
+  }, []);
+
+  return true;
+};
+
+export default LocationListener;
